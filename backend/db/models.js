ff5d46897e16798095322465acc478ae9a185547
tech4GT <varun.gupta1798@gmail.com>
2017-08-25 05:24:44 +0530

    added models
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- backend/db/models.js
+++ backend/db/models.js
@@ -13,3 +13,65 @@ const sequelize = new Sequelize('db', 'user', 'pass', {
         idle: 1000
     }
 });
+
+const courses = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+    desc : Sequelize.STRING
+});
+const batches = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+    startDate : Sequelize.DATE,
+    endDate : Sequelize.DATE,
+    size : Sequelize.INTEGER
+});
+const lectures = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+    date : Sequelize.DATE,
+    startTime : Sequelize.TIME,
+    endTime : Sequelize.TIME,
+    topic : Sequelize.STRING
+});
+const rooms = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+    capacity : Sequelize.INTEGER
+});
+const centres = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+});
+const user = sequelize.define({
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name : Sequelize.STRING,
+    email : Sequelize.STRING
+});
+
+batches.belongsTo(courses)
+courses.hasMany(batches)
+
+lectures.belongsTo(batches)
+batches.hasMany(lectures)
+
+batches.belongsTo(centres)
+centres.hasMany(batches)
+
+rooms.belongsTo(centres)
+centres.hasMany(rooms)
+
+lectures.belongsTo(rooms)
+rooms.hasMany(lectures)
+
+
+module.exports = {
+   courses,
+    batches,
+    lectures,
+    rooms,
+    user,
+    centres,
+}
+
+sequelize.sync()
