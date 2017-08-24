7a367caeb23147a00beae1ab768dcd55f388f35d
aayusharora <aayush113002@gmail.com>
2017-08-25 04:24:15 +0530

    Added calender

---
+++ src/js/calendar.js
@@ -0,0 +1,12 @@
+/**
+ * Created by aayusharora on 8/25/17.
+ */
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
