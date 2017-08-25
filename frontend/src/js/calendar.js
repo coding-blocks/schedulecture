37d6fd3b344cfc26ba377fc930f5471c7e971a75
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-08-25 05:39:52 +0530

    Add fixedWeekCount
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- frontend/src/js/calendar.js
+++ frontend/src/js/calendar.js
@@ -1,12 +1,19 @@
 /**
  * Created by aayusharora on 8/25/17.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // page is now ready, initialize the calendar...
+  // page is now ready, initialize the calendar...
 
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
+  $('#calendar').fullCalendar({
+    // put your options and callbacks here
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    fixedWeekCount: false
+
+  })
 
 });
