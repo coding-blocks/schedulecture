f376c801c253da1cf04bbe9c0d11091e621d7e0c
tech4GT <varun.gupta1798@gmail.com>
2017-08-25 05:55:20 +0530

    fix
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

---
+++ backend/db/actions/courses.js
@@ -0,0 +1,66 @@
+/**
+ * Created by tech4GT on 8/25/17.
+ */
+
+const models = require('../models')
+
+module.exports = {
+    createNew : function (name,desc,done) {
+        models.courses.create({
+            name : name,
+            desc : desc
+        }).then(function (data) {
+            done(data)
+        }).catch(function (err) {
+            if(err) throw err;
+        });
+    },
+    getAll : function (done) {
+        models.courses.findAll({
+        }).then(function (data) {
+            done(data)
+        }).catch(function (err) {
+            if(err) throw err;
+        });
+    },
+    search : function (id, done) {
+        models.courses.findOne({
+            where : {
+                id : id
+            }
+        }).then(function (data) {
+            done(data)
+        }).catch(function (err) {
+            if(err) throw err;
+        });
+    },
+    edit : function (id,obj, done) {
+        models.courses.findOne({
+            where : {
+                id : id
+            }
+        }).then(function (data) {
+            data.update(obj).then(function (resData) {
+                done({
+                    "status" : resData
+                })
+            }).catch(function (err) {
+                if(err) throw err;
+            })
+        }).catch(function (err) {
+            if(err) throw err;
+        });
+    },
+    delete : function (id, done) {
+        model.courses.destroy({
+            where : {
+                id : id
+            }
+        }).then(function (data) {
+            done(data)
+        }).catch(function (err) {
+            if(err) throw err;
+        });
+    }
+
+}
---
+++ backend/db/actions/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by tech4GT on 8/25/17.
+ */
