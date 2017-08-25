f405c0a733ee7454da08c3145ea64a2f4befd1e3
Abhishek Yadav <abhishekgit1208@gmail.com>
2017-08-25 05:30:23 +0530

    added basic routing

---
+++ backend/routes/api_v1/teachers.js
@@ -0,0 +1,36 @@
+/**
+ * Created by abhishekyadav on 25/08/17.
+ */
+const express = require('express');
+const router = express.Router();
+
+
+route.post('/new',function (req,res) {
+    res.send("posting a batch");
+});
+
+
+route.get('/',function (req,res) {
+    res.send("getting all batches");
+});
+
+
+route.get('/:id',function (req,res) {
+    res.send("getting a batch");
+});
+
+
+
+route.put('/:id',function (req,res) {
+    res.send("putting a batch");
+
+});
+
+route.delete('/:id',function (req,res) {
+    res.send("deleting a batch");
+
+});
+
+
+
+module.exports = router;
