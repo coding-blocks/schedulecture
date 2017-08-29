dad39add496b6d224900731a369ef7aa4ac005aa
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-08-29 23:08:00 +0530

    Add Fullcalendar-schedulecture
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- frontend/src/js/calendar.js
+++ frontend/src/js/calendar.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
     lecture.each(function () {
       $(this).data('event', {
         title: "Lecture:1",
+        resourceId: 'a',
         start: $.fullCalendar.moment()
       });
 
@@ -30,6 +31,7 @@ $(document).ready(function () {
 
   $('#calendar').fullCalendar({
     // put your options and callbacks here
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
     header: {
       left: 'prev,next today',
       center: 'title',
@@ -40,6 +42,21 @@ $(document).ready(function () {
     droppable: true,
     height: 500,
     events: events,
+    resources: [
+      {
+        id: 'a', title: 'A',eventColor: 'rgb(255,0,0)'
+
+      },
+      {
+        id: 'b', title: 'B',eventColor: 'rgb(0,255,0)'
+      },
+      {
+        id: 'c', title: 'C',eventColor: 'rgb(0,0,255)'
+      },
+      {
+        id: 'd', title: 'D',eventColor: 'rgb(255,0,255)'
+      }
+    ],
     eventDrop: function (event, delta, revertFunction, jsEvent, ui, view) {
       console.log("1");
       if (isEventOverDiv(jsEvent.clientX, jsEvent.clientY)) {
