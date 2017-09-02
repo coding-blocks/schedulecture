0db3a5ba889dbecd23fb1d911149b2bdad04013d
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-09-02 15:22:11 +0530

    Change structure
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bp.urlencoded({extended : true}))
 // const api_v1 = require('./backend/routes/api_v1')
 
 
-app.use('/', express.static(__dirname + "/frontend"));
+app.use('/', express.static(__dirname + "/public_html"));
 // app.use('/api/v1',api_v1)
-app.use('/bower_components', express.static(__dirname + "/bower_components"));
+// app.use('/bower_components', express.static(__dirname + "/bower_components"));
 app.listen(process.env.PORT || 4000, function () {
   console.log(`Server listening at ` + (process.env.PORT || 4000));
 });
