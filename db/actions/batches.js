c06556c1b515d8d33921a0e647f438e03836e240
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-09-27 18:15:44 +0530

    Save Events After Dropping
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/actions/batches.js
+++ db/actions/batches.js
@@ -5,101 +5,101 @@
 const models = require('../models')
 module.exports = {
 
-    newBatch: function (name, startDate, endDate, size, courseId, centreId, teacherId, done) {
-        models.Batches.create({
-            name: name,
-            startDate: startDate,
-            endDate: endDate,
-            size: size,
-            status : "active",
-            courseId : courseId,
-            centreId : centreId,
-            teacherId : teacherId
-        }).then(function (data) {
-            done(null,data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    getAll: function (conditions, done) {
-        models.Batches.findAll({
-            where : conditions
-        }).then(function (data) {
-            done(null,data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    search: function (id, done) {
-        models.Batches.findOne({
-            where: {
-                id: id
-            }
-        }).then(function (data) {
-            done(null,data);
-        }).
-        catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    edit : function (id,obj, done) {
-        models.Batches.findOne({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            if(!data){
-                return done(null,null)
-            }
-            data.update(obj).then(function (resData) {
-                done(null,resData);
-            }).catch(function (err) {
-                if(err) done(err);
-            })
-        }).catch(function (err) {
-            if(err) done(err);
-        });
-    },
-    archiveBatch : function (id, done) {
-        models.batches.findOne({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            if(!data){
-                return done(null,null)
-            }
-            data.update({
-                status : "archived"
-            }).then(function (resData) {
-                done(null,resData);
-            }).catch(function (err) {
-                if(err) done(err);
-            })
-        }).catch(function (err) {
-            if(err) done(err);
-        });
-    },
-    deleteBatch : function (id, done) {
-        models.Batches.destroy({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            done(null,data)
-        }).catch(function (err) {
-            if(err) done(err);
-        });
-    },
-    getLectures : function (id, done) {
-        models.Lectures.findAll({
-            where : {
-                batchId : id
-            }
-        }).then(function (data) {
-            done(null,data)
-        }).catch(function (err) {
-            if(err) done(err);
-        });
-    }
+  newBatch: function (name, startDate, endDate, size, courseId, centreId, teacherId, done) {
+    models.Batches.create({
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+      size: size,
+      status: "active",
+      courseId: courseId,
+      centreId: centreId,
+      teacherId: teacherId
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  getAll: function (conditions, done) {
+    models.Batches.findAll({
+      where: conditions,
+      include: models.Lectures
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  search: function (id, done) {
+    models.Batches.findOne({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      done(null, data);
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  edit: function (id, obj, done) {
+    models.Batches.findOne({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      if (!data) {
+        return done(null, null)
+      }
+      data.update(obj).then(function (resData) {
+        done(null, resData);
+      }).catch(function (err) {
+        if (err) done(err);
+      })
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  archiveBatch: function (id, done) {
+    models.batches.findOne({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      if (!data) {
+        return done(null, null)
+      }
+      data.update({
+        status: "archived"
+      }).then(function (resData) {
+        done(null, resData);
+      }).catch(function (err) {
+        if (err) done(err);
+      })
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  deleteBatch: function (id, done) {
+    models.Batches.destroy({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  getLectures: function (id, done) {
+    models.Lectures.findAll({
+      where: {
+        batchId: id
+      }
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  }
 }
--- public_html/admin/index.js
+++ public_html/admin/index.js
@@ -30,58 +30,167 @@ $(document).ready(function () {
 
         batches.forEach(function (batch) {
           const $lectures = $(`#batch-${batch.name}`);
-          $.get(`${api}/batches/${batch.id}/lectures`).done((data) => {
-            if (data.success) {
-              let lectures = data.data;
-              // console.log(lectures)
-              lectures.forEach((lecture) => {
-                if (lecture.startTime && lecture.endTime) {
-                  // console.log(moment(lecture.startTime))
-                  // console.log(lecture.endTime)
-                  events.push({
-                    lectureId: lecture.id,
-                    title: lecture.name,
-                    start: moment(lecture.startTime),
-                    end: moment(lecture.endTime),
-                    stick: true,
-                    resourceId: 'a'
-                  })
-                  console.log(11111)
-                  console.log(events)
-                  // $('#calendar').fullCalendar('refetchEvents');
-                } else {
 
-                  $lectures.append(`
+
+            let lectures = batch.lectures;
+            // console.log(lectures)
+            lectures.forEach((lecture) => {
+              if (lecture.startTime && lecture.endTime) {
+                // console.log(moment(lecture.startTime))
+                // console.log(lecture.endTime)
+                events.push({
+                  lectureId: lecture.id,
+                  title: lecture.name,
+                  start: moment.utc(lecture.startTime),
+                  end: moment.utc(lecture.endTime),
+                  stick: true,
+                  resourceId: 'a'
+                })
+                console.log(11111)
+                console.log(events)
+                // $('#calendar').fullCalendar('refetchEvents');
+              } else {
+
+                $lectures.append(`
                     <a id="lecture-${lecture.id}" class="list-group-item" data-parent="#batch-${batch.name}" draggable="true">
                         ${lecture.name}
                     </a>
                   `);
 
 
-                  const $lecture = $(`#lecture-${lecture.id}`);
+                const $lecture = $(`#lecture-${lecture.id}`);
 
-                  $lecture.data('event', {
-                    lectureId: lecture.id,
-                    title: lecture.name,
-                    start: moment().startOf('day'),
-                    stick: true,
-                    resourceId: 'a'
-                  });
+                $lecture.data('event', {
+                  lectureId: lecture.id,
+                  title: lecture.name,
+                  start: moment().startOf('day'),
+                  stick: true,
+                  resourceId: 'a'
+                });
 
-                  $lecture.draggable({
-                    zIndex: 999,
-                    revert: true,
-                    revertDuration: 0
-                  })
-                }
+                $lecture.draggable({
+                  zIndex: 999,
+                  revert: true,
+                  revertDuration: 0
+                })
+              }
 
-              })
-            } else {
-              console.log('No Lectures for batch ' + batch.name);
+            })
+
+        });
+
+
+        $('#calendar').fullCalendar({
+          // put your options and callbacks here
+          schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+          header: {
+            left: 'prev,next,today',
+            center: 'title',
+            right: 'month,agendaSevenDay,agendaFiveDay,agendaTwoDay,agendaOneDay'
+          },
+          fixedWeekCount: false,
+          editable: true,
+          droppable: true,
+          minTime: "07:00:00",
+          maxTime: "22:00:00",
+          //height: 500,
+          events: events,
+          dayClick: function (date, jsEvent, view, resourceObj) {
+            $('#calendar').fullCalendar('changeView', 'agendaOneDay', date);
+          },
+          resources: [
+            {
+              id: 'a', title: 'A', eventColor: 'rgb(255,0,0)'
+
+            },
+            {
+              id: 'b', title: 'B', eventColor: 'rgb(0,255,0)'
+            },
+
+            {
+              id: 'c', title: 'C', eventColor: 'rgb(0,0,255)'
+            },
+            {
+              id: 'd', title: 'D', eventColor: 'rgb(255,0,255)'
             }
-          }).fail((err) => {
-            console.log(err)
-          })
+          ],
+          views: {
+            agendaFiveDay: {
+              type: 'agenda',
+              duration: {days: 5},
+              groupByDateAndResource: true,
+              buttonText: '5 days',
+              dateIncrement: {days: 1}
+            },
+            agendaTwoDay: {
+              type: 'agenda',
+              duration: {days: 2},
+              groupByDateAndResource: true,
+              buttonText: '2 days',
+              dateIncrement: {days: 1}
+            },
+            agendaOneDay: {
+              type: 'agenda',
+              duration: {days: 1},
+              groupByDateAndResource: true,
+              buttonText: 'day',
+              dateIncrement: {days: 1}
+            },
+            agendaSevenDay: {
+              type: 'agenda',
+              duration: {days: 7},
+              groupByDateAndResource: true,
+              dateIncrement: {days: 1}
+            }
+
+          },
+          eventDrop: function (event, delta, revertFunction, jsEvent, ui, view) {
+            console.log("1");
+            console.log(event);
+            $.ajax({
+              method: 'PUT',
+              url: `${api}/lectures/${event.lectureId}`,
+              data: {
+                values: {
+                  startTime: event.start._d,
+                  endTime: event.end._d,
+                  date: event.start._d
+                }
+              }
+            }).done(function (data) {
+              console.log(data)
+            }).fail(function (err) {
+              console.log(err);
+            })
+          },
+          drop: function (date, jsEvent, ui, resourceId) {
+            // console.log(2);
+            $(this).remove();
+
+          },
+          eventReceive: function (event) {
+            // console.log(event);
+            event.start = event.start.add(10, 'hours');
+            event.end = moment(event.start).add(3, 'hours');
+            // console.log(event.start._d.getDay())
+            $('#calendar').fullCalendar('updateEvent', event);
+            $.ajax({
+              method: 'PUT',
+              url: `${api}/lectures/${event.lectureId}`,
+              data: {
+                values: {
+                  startTime: event.start._d,
+                  endTime: event.end._d,
+                  date: event.start._d
+                }
+              }
+            }).done(function (data) {
+              console.log(data)
+            }).fail(function (err) {
+              console.log(err);
+            })
+          }
+
         });
 
       } else {
@@ -96,105 +205,6 @@ $(document).ready(function () {
 
   // page is now ready, initialize the calendar...
   console.log(222222222)
-  $('#calendar').fullCalendar({
-    // put your options and callbacks here
-    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-    header: {
-      left: 'prev,next,today',
-      center: 'title',
-      right: 'month,agendaSevenDay,agendaFiveDay,agendaTwoDay,agendaOneDay'
-    },
-    fixedWeekCount: false,
-    editable: true,
-    droppable: true,
-    minTime: "07:00:00",
-    maxTime: "22:00:00",
-    //height: 500,
-    events: events,
-    dayClick: function (date, jsEvent, view, resourceObj) {
-      $('#calendar').fullCalendar('changeView', 'agendaOneDay', date);
-    },
-    resources: [
-      {
-        id: 'a', title: 'A', eventColor: 'rgb(255,0,0)'
-
-      },
-      {
-        id: 'b', title: 'B', eventColor: 'rgb(0,255,0)'
-      },
-
-      {
-        id: 'c', title: 'C', eventColor: 'rgb(0,0,255)'
-      },
-      {
-        id: 'd', title: 'D', eventColor: 'rgb(255,0,255)'
-      }
-    ],
-    views: {
-      agendaFiveDay: {
-        type: 'agenda',
-        duration: {days: 5},
-        groupByDateAndResource: true,
-        buttonText: '5 days',
-        dateIncrement: {days: 1}
-      },
-      agendaTwoDay: {
-        type: 'agenda',
-        duration: {days: 2},
-        groupByDateAndResource: true,
-        buttonText: '2 days',
-        dateIncrement: {days: 1}
-      },
-      agendaOneDay: {
-        type: 'agenda',
-        duration: {days: 1},
-        groupByDateAndResource: true,
-        buttonText: 'day',
-        dateIncrement: {days: 1}
-      },
-      agendaSevenDay: {
-        type: 'agenda',
-        duration: {days: 7},
-        groupByDateAndResource: true,
-        dateIncrement: {days: 1}
-      }
-
-    },
-    eventDrop: function (event, delta, revertFunction, jsEvent, ui, view) {
-      console.log("1");
-      console.log(event);
-      console.log(($('#calendar').fullCalendar('clientEvents')));
-
-    },
-    drop: function (date, jsEvent, ui, resourceId) {
-      // console.log(2);
-      $(this).remove();
-
-    },
-    eventReceive: function (event) {
-      // console.log(event);
-      event.start = event.start.add(10, 'hours');
-      event.end = moment(event.start).add(3, 'hours');
-      // console.log(event.start._d.getDay())
-      $('#calendar').fullCalendar('updateEvent', event);
-      $.ajax({
-        method: 'PUT',
-        url: `${api}/lectures/${event.lectureId}`,
-        data: {
-          values: {
-            startTime: event.start._d,
-            endTime: event.end._d,
-            date: event.start._d
-          }
-        }
-      }).done(function (data) {
-        console.log(data)
-      }).fail(function (err) {
-        console.log(err);
-      })
-    }
-
-  });
 
 
 });
