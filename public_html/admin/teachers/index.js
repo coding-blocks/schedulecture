f3977bdbd7a0a38611589036a2c2a03a43308986
apoorvaagupta <apoorvaagupta01@gmail.com>
2017-10-01 00:36:51 +0530

    added add, edit, delete functionality for teachers
    Signed-off-by: apoorvaagupta <apoorvaagupta01@gmail.com>

--- public_html/admin/teachers/index.js
+++ public_html/admin/teachers/index.js
@@ -14,13 +14,66 @@ $(document).ready(function () {
                     <div class="text-center"  style="padding: 15px 0">
                         <h3>`+teachers.data[i].name +`</h3>
                         <p>Email: `+teachers.data[i].email +`<br> Contact: `+teachers.data[i].contact +`</p>
-                        <i class="fa fa-pencil fa-pencil-right" style="color: blue; font-size: 24px"></i>&nbsp;
-                        <i class="fa fa-trash-o" style="color: red; font-size: 24px"></i>
+                        <i class="fa fa-pencil edit" style="color: blue; font-size: 24px"  teacher-id="` + teachers.data[i].id + `"></i>&nbsp;
+                        <i class="fa fa-trash-o delete" style="color: red; font-size: 24px" teacher-id="` + teachers.data[i].id + `"></i>
 
                     </div>
                 </div>
             </li>`)
       }
+
+      $('.edit').click(function (e) {
+        let teacherId = e.target.getAttribute('teacher-id');
+        $.get('http://localhost:4000/api/v1/teachers/' + teacherId, function (teacher) {
+          if (teacher.success === true) {
+            $('#editTeacherName').val(teacher.data.name);
+            $('#EditTeacherEmail').val(teacher.data.email);
+            $('#editTeacherContact').val(teacher.data.contact);
+
+            $('#editTeachersModal').modal('show');
+
+            $('#editTeacherSave').click(function () {
+              let name = $('#editTeacherName').val();
+              let email = $('#EditTeacherEmail').val();
+              let contact = $('#editTeacherContact').val();
+              $.ajax({
+
+                url: 'http://localhost:4000/api/v1/teachers/' + teacherId,
+                data: {
+                  values: {
+                    name: name,
+                    email: email,
+                    contact: contact
+                  }
+                },
+                method: 'PUT'
+              }).done(function (teacher) {
+                if (teacher.success === true) {
+
+                  $('#editTeachersModal').modal('hide');
+                  window.location.reload();
+                }
+                else {
+                  console.log("could not edit the teacher right now")
+                }
+              });
+            })
+          }
+        })
+      })
+      $('.delete').click(function (e) {
+        let teacherId = e.target.getAttribute('teacher-id');
+        $.ajax({
+          url: 'http://localhost:4000/api/v1/teachers/' + teacherId,
+          method: 'DELETE'
+        }).done(function (res) {
+          if(res.success === true){
+            window.location.reload();
+          }else{
+            window.alert('Could Not Delete The Teacher Right Now!')
+          }
+        })
+      })
     }
   })
 
