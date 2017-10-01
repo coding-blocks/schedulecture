7698d2cdc8ba1f1ec87d710cc5d79e2e4e0ffd90
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-10-01 18:38:30 +0530

    Update Centre DB Actions
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/actions/centres.js
+++ db/actions/centres.js
@@ -4,87 +4,87 @@
 const models = require('../models')
 
 module.exports = {
-    createNew: function (name, head, phone, done) {
-        models.Centres.create({
-            name: name,
-            head : head,
-            phone : phone
-        }).then(function (data) {
-            done(null, data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    getAll : function (done) {
-        models.Centres.findAll({
-        }).then(function (data) {
-            done(null, data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    search : function (id, done) {
-        models.Centres.findOne({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            done(null, data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    edit : function (id,obj, done) {
-        models.Centres.findOne({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            if (!data) {
-                return done(null, null)
-            }
-            data.update(obj).then(function (resData) {
-                done(null, resData);
-            }).catch(function (err) {
-                if (err) done(err);
-            })
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    deleteCentre : function (id, done) {
-        models.Centres.destroy({
-            where : {
-                id : id
-            }
-        }).then(function (data) {
-            done(null, data)
-        }).catch(function (err) {
-            if (err) done(err);
-        });
-    },
-    getBatches : function(id, done) {
-        models.Batches.findAll({
-            where : {
-                centreId : id
-            }
-        }).then(function(data){
-            done(null, data);
-        }).catch(function(err){
-            if(err)
-                done(err);
-        })
-    },
-    getRooms : function (id, done) {
-        models.Rooms.findAll({
-            where : {
-                centreId : id
-            }
-        }).then(function(data) {
-            done(null, data);
-        }).catch(function(err) {
-            if(err)
-                done(err);
-        })
-    }
+  createNew: function (name, head, phone, done) {
+    models.Centres.create({
+      name: name,
+      head: head,
+      phone: phone
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  getAll: function (done) {
+    models.Centres.findAll({}).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  search: function (id, done) {
+    models.Centres.findOne({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  edit: function (id, obj, done) {
+    models.Centres.findOne({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      if (!data) {
+        return done(null, null)
+      }
+      data.update(obj).then(function (resData) {
+        done(null, resData);
+      }).catch(function (err) {
+        if (err) done(err);
+      })
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  deleteCentre: function (id, done) {
+    models.Centres.destroy({
+      where: {
+        id: id
+      }
+    }).then(function (data) {
+      done(null, data)
+    }).catch(function (err) {
+      if (err) done(err);
+    });
+  },
+  getBatches: function (id, done) {
+    models.Batches.findAll({
+      where: {
+        centreId: id
+      },
+      include:[models.Lectures]
+    }).then(function (data) {
+      done(null, data);
+    }).catch(function (err) {
+      if (err)
+        done(err);
+    })
+  },
+  getRooms: function (id, done) {
+    models.Rooms.findAll({
+      where: {
+        centreId: id
+      }
+    }).then(function (data) {
+      done(null, data);
+    }).catch(function (err) {
+      if (err)
+        done(err);
+    })
+  }
 }
