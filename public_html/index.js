ca031c9e863403e77066d979c5d0a8bd53bed2c1
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-10-02 11:53:19 +0530

    Add View Only Support
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ public_html/index.js
@@ -0,0 +1,208 @@
+/**
+ * Created by bhavyaagg on 01/10/17.
+ */
+
+$(document).ready(function () {
+
+
+  var api = 'http://localhost:4000/api/v1';
+
+  function getCentres() {
+
+    $.get(`${api}/centres`).done((centresData) => {
+      if (centresData) {
+        const centres = centresData.data;
+        const $centres = $("#centres");
+        centres.forEach((centre) => {
+          $centres.append(`
+            <option value="${centre.id}">${centre.name}</option>
+          `);
+        });
+        showBatchesAndRooms(centres[0]);
+
+        $centres.change(() => {
+          getCentre(+($centres.val()));
+        })
+      } else {
+        alert('No Centres.');
+      }
+    }).fail((err) => {
+      alert('No Centres.');
+      console.log(err);
+    });
+
+  }
+
+  getCentres();
+
+  function getCentre(centreId) {
+    $.get(`${api}/centres/${centreId}`).done((centreData) => {
+      if (centreData.success) {
+        showBatchesAndRooms(centreData.data);
+      } else {
+        alert('Unknown Centre')
+      }
+    }).fail((err) => {
+      alert('Unknown Centre.');
+      console.log(err);
+    });
+  }
+
+  function showBatchesAndRooms(centre) {
+
+    const events = [];
+    const resources = [];
+    const colors = ['rgb(255,0,0)', 'rgb(0,255,0)', 'rgb(0,0,255)', 'rgb(255,0,255)', 'rgb(255,255,0)', 'rgb(0,255,255)'];
+    let colorCounter = 0;
+
+    $('#calendar').remove();
+
+    $('#calendarContainer').append(`
+    <div id="calendar"></div>
+    `);
+
+    $.get(`${api}/centres/${centre.id}/rooms`).done((roomsData) => {
+      if (roomsData.success) {
+
+        const rooms = roomsData.data;
+        const $colors = $('#colors');
+        $colors.empty().append(`
+              <div class="col">
+              </div>
+              <div class="col">
+              </div>
+          `);
+
+        rooms.forEach((room) => {
+          let currentColor = colors[colorCounter++ % colors.length];
+          resources.push({
+            id: room.id,
+            title: room.name,
+            eventColor: currentColor,
+            capacity: room.capacity,
+            centreId: room.centreId
+          });
+          $colors.append(`
+              <div class="col">
+                <div style="display: inline-block;height: 15px;width: 15px; background-color: ${currentColor}"></div>
+                <span>${room.name}</span>
+              </div>
+          `)
+        });
+
+        $colors.append(`
+              <div class="col">
+              </div>
+              <div class="col">
+              </div>
+          `);
+
+        $.get(`${api}/centres/${centre.id}/batches`).done((data) => {
+          if (data.success) {
+            let batches = data.data;
+
+            batches.forEach(function (batch) {
+
+              let lectures = batch.lectures;
+              lectures.forEach((lecture) => {
+                if (lecture.startTime && lecture.endTime) {
+                  events.push({
+                    lectureId: lecture.id,
+                    title: lecture.name,
+                    start: moment.utc(lecture.startTime),
+                    end: moment.utc(lecture.endTime),
+                    stick: true,
+                    resourceId: lecture.roomId
+                  });
+                }
+              });
+
+            });
+
+            $('#calendar').fullCalendar({
+              // put your options and callbacks here
+              schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+              header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month,agendaSevenDay,agendaFiveDay,agendaTwoDay,agendaOneDay'
+              },
+              fixedWeekCount: false,
+              minTime: "07:00:00",
+              maxTime: "22:00:00",
+              height: 800,
+              events: events,
+              dayClick: function (date, jsEvent, view, resourceObj) {
+                $('#calendar').fullCalendar('changeView', 'agendaOneDay', date);
+              },
+              resources: resources,
+              views: {
+                agendaFiveDay: {
+                  type: 'agenda',
+                  duration: {days: 5},
+                  groupByDateAndResource: true,
+                  buttonText: '5 days',
+                  dateIncrement: {days: 1}
+                },
+                agendaTwoDay: {
+                  type: 'agenda',
+                  duration: {days: 2},
+                  groupByDateAndResource: true,
+                  buttonText: '2 days',
+                  dateIncrement: {days: 1}
+                },
+                agendaOneDay: {
+                  type: 'agenda',
+                  duration: {days: 1},
+                  groupByDateAndResource: true,
+                  buttonText: 'day',
+                  dateIncrement: {days: 1}
+                },
+                agendaSevenDay: {
+                  type: 'agenda',
+                  duration: {days: 7},
+                  groupByDateAndResource: true,
+                  dateIncrement: {days: 1}
+                }
+
+              }
+            });
+
+          } else {
+            alert('There are no Batches');
+          }
+        }).fail((err) => {
+          alert('No Batches At This Centre.');
+          console.log(err)
+        })
+      } else {
+        alert('No Rooms At This Centre.');
+      }
+
+    }).fail((err) => {
+      alert('No Rooms At This Centre.');
+      console.log(err)
+    });
+  }
+
+  function updateLecture(event) {
+    $.ajax({
+      method: 'PUT',
+      url: `${api}/lectures/${event.lectureId}`,
+      data: {
+        values: {
+          startTime: event.start._d,
+          endTime: event.end._d,
+          date: event.start._d,
+          roomId: +event.resourceId
+        }
+      }
+    }).done(function (data) {
+      console.log(data)
+    }).fail(function (err) {
+      alert('Could Not Update The Lecture');
+      console.log(err);
+    })
+  }
+
+});
