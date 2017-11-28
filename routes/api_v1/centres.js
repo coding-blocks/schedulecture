cf5ded7fc0b9b25d9c2769662e05be114557baba
Abhishek Yadav <abhishekgit1208@gmail.com>
2017-11-28 20:31:35 +0530

    added link to button

--- routes/api_v1/centres.js
+++ routes/api_v1/centres.js
@@ -6,6 +6,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db');
 const acl=require('../../utils/acl');
+const passport=require('../../auth/passport');
+
 
 
 
