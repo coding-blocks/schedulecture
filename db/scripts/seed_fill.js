7d3f7147e26d5c96eed9717fb2e699f678ecda19
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-08 12:45:28 +0530

    Add Inital Seed Files
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ db/scripts/seed_fill.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bhavyaagg on 30/12/17.
+ */
+
+const models = require('./../models');
+const seed = require('./../seed');
+
+models.DATABASE.sync({
+  force: true,
+}).then(function () {
+  console.log("Database Configured");
+
+  models.Teachers.bulkCreate(seed.teachers).then(function (r) {
+    console.log(r.map(v => v.get()));
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+});
+
+
---
+++ db/seed/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bhavyaagg on 30/12/17.
+ */
+
+module.exports = exports = {
+  teachers: require('./teachers')
+};
