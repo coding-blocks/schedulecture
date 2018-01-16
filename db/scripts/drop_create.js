c8911e758c8d39588f7b1070c2802fbd76ae484d
varunzxzx <varun995862@gmail.com>
2018-01-16 18:27:27 +0530

    fix db recreate

--- db/scripts/drop_create.js
+++ db/scripts/drop_create.js
@@ -1,1 +1,9 @@
-require('../models')
+const models = require('./../models');
+
+models.DATABASE.sync({
+    force: true,
+}).then(function() {
+    console.log("Database Recreated")
+}).catch(function (err) {
+    console.log(err);
+})
