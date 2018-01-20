e56e6f712805146f6fc09bb204c0870e03d2d5c0
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-20 17:00:19 +0530

    Add Time Picker
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public_html/admin/batches/index.js
+++ public_html/admin/batches/index.js
@@ -7,6 +7,12 @@ $(document).ready(function () {
   $('#endDate').datepicker();
   $('#editStartDate').datepicker();
   $('#editEndDate').datepicker();
+  $('#defaultTime').datetimepicker({
+    format: 'LT'
+  });
+  $('#editdefaultTime').datetimepicker({
+    format: 'LT'
+  });
 
   let centreId, statusId, courseId;
   let url = '/api/v1/batches'
