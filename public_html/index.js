878659f40d9393894443ac9c171929d7fa6a7aa4
Kapil <kapillamba4@gmail.com>
2018-07-07 21:10:30 +0530

    Allow Anonymous users to set filters

--- public_html/index.js
+++ public_html/index.js
@@ -18,6 +18,11 @@ $(document).ready(function () {
   }
 
   var api = '/api/v1';
+  var eventsData;
+
+  getCentres();
+
+  $('#filters').show();
 
   function getCentres() {
 
@@ -31,9 +36,11 @@ $(document).ready(function () {
           `);
         });
         showBatchesAndRooms(centres[0]);
+        getFilters(centres[0].id);
 
         $centres.change(() => {
           getCentre(+($centres.val()));
+          getFilters(+($centres.val()));
         })
       } else {
         alert('No Centres.');
@@ -44,8 +51,6 @@ $(document).ready(function () {
     });
 
   }
-
-  getCentres();
 
   function getCentre(centreId) {
     $.get(`${api}/centres/${centreId}`).done((centreData) => {
@@ -60,11 +65,126 @@ $(document).ready(function () {
     });
   }
 
+  function getFilters(centreId) {
+
+    const filters = $('#filters');
+    filters.empty();
+
+    let teachersString = "";
+    let batchesString = "";
+    let roomsString = "";
+
+    $.get(`${api}/teachers`, function (teachersData) {
+      if (teachersData.success && teachersData.data.length > 1) {
+        teachersString = `<div class="filter-column-divs">
+                    <div class="filter-column-divs-heading">Teachers</div>
+                    <div class="filter-column-divs-content">`;
+        for (let i = 0; i < teachersData.data.length; i++) {
+          teachersString += `
+                        <label class="label-style">
+                            <input class="checkbox-style" name="teachers" value="` + teachersData.data[i].id + `" type="checkbox">&nbsp;&nbsp;` + teachersData.data[i].name + `<br/>
+                        </label>
+                        <br>
+                    `;
+        }
+        teachersString += `</div></div>`;
+        filters.append(teachersString);
+
+      }
+
+      $.get(`${api}/centres/${centreId}/batches/active`, function (batchesData) {
+        if (batchesData.success && batchesData.data.length > 1) {
+          batchesString = `<div class="filter-column-divs">
+                    <div class="filter-column-divs-heading">Batches</div>
+                    <div class="filter-column-divs-content">`;
+          for (let i = 0; i < batchesData.data.length; i++) {
+            batchesString += `
+                        <label class="label-style">
+                            <input class="checkbox-style" name="batches" value="` + batchesData.data[i].id + `" type="checkbox">&nbsp;&nbsp;` + batchesData.data[i].name + `<br/>
+                        </label>
+                        <br>
+                    `;
+          }
+          batchesString += `</div></div>`;
+          filters.append(batchesString);
+
+        }
+
+        $.get(`${api}/centres/${centreId}/rooms`, function (roomsData) {
+          if (roomsData.success && roomsData.data.length > 1) {
+            roomsString = `<div class="filter-column-divs">
+                    <div class="filter-column-divs-heading">Rooms</div>
+                    <div class="filter-column-divs-content">`;
+            for (let i = 0; i < roomsData.data.length; i++) {
+              roomsString += `
+                        <label class="label-style">
+                            <input class="checkbox-style" name="rooms" value="` + roomsData.data[i].id + `" type="checkbox">&nbsp;&nbsp;` + roomsData.data[i].name + `<br/>
+                        </label>
+                        <br>
+                    `;
+            }
+            roomsString += `</div></div>`;
+            filters.append(roomsString);
+
+          }
+          $('input[type="checkbox"]').off('change');
+
+          $('input[type="checkbox"]').change(function () {
+
+            let teachersArray = [];
+            let batchesArray = [];
+            let roomsArray = [];
+            let count = 0;
+
+            const teachersFilters = $('input[name="teachers"]');
+            const batchesFilters = $('input[name="batches"]');
+            const roomsFilters = $('input[name="rooms"]');
+
+            for (let i = 0; i < teachersFilters.length; i++) {
+              if (teachersFilters[i].checked) {
+                teachersArray.push(+teachersFilters[i].value);
+                count++;
+              }
+            }
+
+            for (let i = 0; i < batchesFilters.length; i++) {
+              if (batchesFilters[i].checked) {
+                batchesArray.push(+batchesFilters[i].value);
+                count++;
+              }
+            }
+
+            for (let i = 0; i < roomsFilters.length; i++) {
+              if (roomsFilters[i].checked) {
+                roomsArray.push(+roomsFilters[i].value);
+                count++;
+              }
+            }
+
+            let filteredevents = eventsData.filter(function (event) {
+              if ((teachersArray.length === 0 || teachersArray.includes(+event.teacherId)) &&
+                (roomsArray.length === 0 || roomsArray.includes(+event.resourceId)) &&
+                (batchesArray.length === 0 || batchesArray.includes(event.batchId)))
+                return true;
+              else
+                return false;
+            })
+
+            $('#calendar').fullCalendar('removeEventSources');
+            $('#calendar').fullCalendar('addEventSource', filteredevents);
+
+          })
+        });
+      });
+    });
+  }
+
   function showBatchesAndRooms(centre) {
 
     const events = [];
-    const resources = [];
+    const resources = []
     const colors = ['#EB5667', '#1E88E5', '#B96BC6', '#28B294', '#FF8F00', '#31C6C7'];
+    let batchColors = {};
     let colorCounter = 0;
 
     $('#calendar').remove();
@@ -97,44 +217,71 @@ $(document).ready(function () {
           $colors.append(`
               <div class="col">
                 <div style="display: inline-block;height: 15px;width: 15px; background-color: ${currentColor}"></div>
-                <span>${room.name}</span>
+                <span>${room.name} (${room.capacity})</span>
               </div>
           `)
         });
 
         $colors.append(`
-              <div class="col">
-              </div>
-              <div class="col">
-              </div>
-          `);
+          <div class="col">
+          </div>
+          <div class="col">
+          </div>
+        `);
 
-        $.get(`${api}/centres/${centre.id}/batches`).done((data) => {
+        $.get(`${api}/centres/${centre.id}/batches/active`).done((data) => {
+
           if (data.success) {
             let batches = data.data;
 
             batches.forEach(function (batch) {
-
-              let lectures = batch.lectures;
+              
+              const $lectures = $(`#batch-${batch.id}`);
+              let lectures = batch.lectures.sort(function (batch1, batch2) {
+                return batch1.id - batch2.id;
+              });
               lectures.forEach((lecture) => {
+                let eventObject = {
+                  lectureId: lecture.id,
+                  title: lecture.name,
+                  batchCapacity: batch.size,
+                  batchName: batch.name,
+                  batchId: batch.id,
+                  teacherId: batch.teacher.id,
+                  teacherName: batch.teacher.name,
+                  courseName: batch.course.name,
+                  stick: true
+                };
+
                 if (lecture.startTime && lecture.endTime) {
-                  events.push({
-                    lectureId: lecture.id,
-                    title: lecture.name,
+
+                  events.push(Object.assign({}, eventObject, {
                     start: moment.utc(lecture.startTime),
                     end: moment.utc(lecture.endTime),
-                    stick: true,
                     resourceId: lecture.roomId,
-                    batchCapacity: batch.size,
-                    batchName: batch.name,
-                    teacherName: batch.teacher.name,
-                    courseName: batch.course.name
-                  });
+                  }));
+
+                } else {
+                  $lectures.append(`
+                    <a id="lecture-${lecture.id}" class="list-group-item" data-parent="#batch-${batch.id}">
+                        ${lecture.name}
+                    </a>
+                  `);
+
+                  const $lecture = $(`#lecture-${lecture.id}`);
+
+                  $lecture.data('event', Object.assign({}, eventObject, {
+                    hours: batch.hoursPerLecture !== null ? batch.hoursPerLecture : batch.course.hours,
+                    start: moment().startOf('day'),
+                    defaultRoom: batch.roomId,
+                    defaultTime: batch.defaultTime,
+                  }));
                 }
               });
 
             });
-
+            $('#calendar').css('overflow-y', 'scroll');
+            eventsData = events.slice();
             $('#calendar').fullCalendar({
               // put your options and callbacks here
               schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
@@ -146,7 +293,7 @@ $(document).ready(function () {
               fixedWeekCount: false,
               minTime: "07:00:00",
               maxTime: "22:00:00",
-              height: 'auto',
+              height: $('#calendarContainer').height() - 60,
               events: events,
               dayClick: function (date, jsEvent, view, resourceObj) {
                 $('#calendar').fullCalendar('changeView', 'agendaOneDay', date);
@@ -180,7 +327,6 @@ $(document).ready(function () {
                   groupByDateAndResource: true,
                   dateIncrement: {days: 1}
                 }
-
               },
               eventMouseover: function (event, jsEvent, view) {
                 var index = -1;
@@ -198,12 +344,14 @@ $(document).ready(function () {
                               Course: ${event.courseName}<br/>
                               Batch: ${event.batchName}<br/>
                               Teacher: ${event.teacherName}<br/>
-                              Batch Capacity: ${event.batchCapacity}<br/>                             
+                              Batch Capacity: ${event.batchCapacity}<br/>
                               Room: ${resources[index].title}<br/>
                     `
                   }).tooltip('show');
               }
+
             });
+
           } else {
             alert('There are no Batches');
           }
@@ -220,4 +368,4 @@ $(document).ready(function () {
       console.log(err)
     });
   }
-});
+})
